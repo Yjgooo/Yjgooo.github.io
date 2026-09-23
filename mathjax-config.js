5f9f0aa173ab5b4b3c5754679683b821cfe168c0
Xi Gong <xi@Xis-MacBook-Air.local>
2026-09-23 16:34:49 -0400

    Update personal website

---
+++ mathjax-config.js
@@ -0,0 +1,8 @@
+// Shared by the blog and future post pages; load before MathJax.
+window.MathJax = {
+    tex: {
+        inlineMath: [['$', '$'], ['\\(', '\\)']],
+        displayMath: [['$$', '$$'], ['\\[', '\\]']],
+        processEscapes: true
+    }
+};
